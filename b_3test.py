3b821d28ea02d89e4704d81fb3bb804f19f5322f
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-08-23 14:46:08 +0700

    'fourth'

--- b_3test.py
+++ b_3test.py
@@ -4,17 +4,14 @@ import cv2 as cv
 def process(src):
     img = cv.imread(src)
     res = cv.imread(src) 
-    rows = len(img)
-    cols = len(img[0])
-    for hang in range(0,4):
-        for cot in range(0,4):
-            i = 3 - hang
-            j = 3 - cot
-            for k in range(0,32):
-                for l in range(0,32):
-                    res[i*32 + k][j*32 + l] = img[hang*32 + k][cot*32 + l]
+    for row in range(0,4):
+        for col in range(0,4):
+            rev_row = 3 - row
+            rev_col = 3 - col
+            for index_row in range(0,32):
+                for index_col in range(0,32):
+                    res[rev_row*32 + index_row][rev_col*32 + index_col] = img[row*32 + index_row][col*32 + index_col]
             
-
     cv.imshow('original img', img)
     cv.imshow('converted img', res)
     cv.waitKey()
